405bac93d0ee57c00fd605c9c4045e37d5306e5e
Arno Appenzeller <arno.appenzeller@gmx.de>
2016-08-01 01:27:16 +0200

    Initial Commit

---
+++ PiPifier/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PiPifier
+//
+//  Created by Arno Appenzeller on 01.08.16.
+//  Copyright © 2016 APPenzeller. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PiPifier/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PiPifier
+//
+//  Created by Arno Appenzeller on 01.08.16.
+//  Copyright © 2016 APPenzeller. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
